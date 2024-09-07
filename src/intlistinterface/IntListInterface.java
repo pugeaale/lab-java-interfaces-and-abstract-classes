81bc1d4d340e8e36345b55a2bc16b6db61b57f22
pugeaale <pugeaale@gmail.com>
2024-09-07 15:31:18 +0200

    :sparkles: testing IntVector implementation

--- src/intlistinterface/IntListInterface.java
+++ src/intlistinterface/IntListInterface.java
@@ -4,13 +4,18 @@ public class IntListInterface {
     public static void main(String[] args) {
         IntArrayList intArrayList = new IntArrayList();
         System.out.println("init : " + intArrayList.getCount());
-        for(int i = 0 ; i < 6 ; i++) {
+        for(int i = 0 ; i < 20 ; i++) {
             intArrayList.add(100*i);
             System.out.println("count : " + intArrayList.getCount());
         }
-        for(int i = 0 ; i <  6 ; i++) {
+        for(int i = 0 ; i <  20 ; i++) {
             System.out.println(intArrayList.get(i));
         }
-
+        IntVector intVectorList = new IntVector();
+        System.out.println("init vector : " + intVectorList.getCount());
+        for(int i = 0 ; i < 22 ; i++) {
+            intVectorList.add(100*i);
+            System.out.println("count : " + intVectorList.getCount());
+        }
     }
 }
